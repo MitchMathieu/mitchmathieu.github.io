9089c5d902c959f0397c6f4d3ab69e3a143546b1
Mitch Mathieu <mitchmathieu35@gmail.com>
2024-04-05 17:55:00 -0400

    Add basic directory navigation commands

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
-const path = require('path');
+const figlet = require('figlet');
 
 const rootDirectory = 'mitchmathieu';
-var currentDirectory = './mitchmathieu';
+var currentDirectory = 'mitchmathieu';
 
 const app = express();
 
@@ -12,7 +12,6 @@ app.use(cors());
 
 app.get('/list-files', (req, res) => {
     const dirPath = currentDirectory;
-
     fs.readdir(dirPath, (err, files) => {
         if (err) {
             res.status(500).send('An error occurred');
@@ -20,6 +19,56 @@ app.get('/list-files', (req, res) => {
             res.json(files);
         }
     });
+});
+
+app.get('/generate-ascii', (req, res) => {
+    const text = req.query.text;
+    figlet.text(text, {
+        font: 'Slant',
+        horizontalLayout: 'default',
+        verticalLayout: 'default'
+    }, function (err, data) {
+        if (err) {
+            res.status(500).send('An error occurred');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.get('/go-to-root-directory', (req, res) => {
+    currentDirectory = rootDirectory;
+    res.send(currentDirectory);
+});
+
+app.get('/change-directory', (req, res) => {
+    const directory = req.query.directory;
+    if (directory === '..') {
+        if (currentDirectory === rootDirectory) {
+            res.status(400).send('Cannot go back');
+            return;
+        }
+        var parts = currentDirectory.split('/');
+        parts.pop();
+        currentDirectory = parts.join('/');
+        res.send(currentDirectory);
+        return;
+    }
+    else {
+        const newDirectory = `${currentDirectory}/${directory}`;
+        fs.stat(newDirectory, (err, stats) => {
+            if (err || !stats.isDirectory()) {
+                res.status(400).send('Invalid directory');
+            } else {
+                currentDirectory = newDirectory;
+                res.send(currentDirectory);
+            }
+        });
+    }
+});
+
+app.get('/pwd', (req, res) => {
+    res.send(currentDirectory);
 });
 
 app.listen(3000, () => console.log('Server started on port 3000'));
--- terminal.js
+++ terminal.js
@@ -1,45 +1,65 @@
 var serverBaseUrl = 'http://localhost:3000';
+var currentDirectory = 'mitchmathieu';
+var count = 0;
 
-$('#terminal').terminal(function (command) {
-    var terminal = this;
-    var commands = {
-        'greet': function () {
-            return 'Hello, user!';
-        },
-        'date': function () {
-            return new Date().toLocaleString();
-        },
-        'ls': function () {
-            return listDirectoriesAndFiles(terminal);
-        },
-        // Add more commands here
-    };
-
-    if (command !== '') {
-        var commandFunction = commands[command];
-        if (commandFunction) {
-            try {
-                var result = commandFunction();
-                if (result !== undefined) {
-                    this.echo(new String(result));
-                }
-            } catch (e) {
-                this.error(new String(e));
-            }
-        }
-        else {
-            this.error('Unknown command: ' + command);
-        }
-    }
-    else {
-        this.echo('');
+$('#terminal').terminal({
+    greet: function () {
+        return 'Hello, user!';
+    },
+    date: function () {
+        return new Date().toLocaleString();
+    },
+    ls: function () {
+        return listDirectoriesAndFiles(this);
+    },
+    cd: function (directory) {
+        changeDirectory(directory, (newDirectory) => {
+            this.set_prompt(`(base) ${newDirectory}$ `);
+        });
+        return;
+    },
+    pwd: function () {
+        return printWorkingDirectory(this);
+    },
+    pcd: function () {
+        this.echo(currentDirectory);
+    },
+    figlet: function (...args) {
+        return generateAsciiArt(args.join(' '));
     }
 }, {
-    greetings: 'Mitch Mathieu - software developer',
+    onInit() {
+        this.echo(generateAsciiArt('Mitch Mathieu'));
+        this.echo(buildGreetingString);
+        goToRootDirectory();
+    },
+    checkArity: false,
+    greetings: false,
     name: 'mitchs-pc',
-    height: 500,
-    prompt: '(base) mitchmathieu$ '
-});
+    prompt: `(base) ${currentDirectory}$ `
+}
+);
+
+function goToRootDirectory() {
+    $.ajax({
+        url: `${serverBaseUrl}/go-to-root-directory`,
+        method: 'GET',
+        success: (newDirectory) => {
+            currentDirectory = newDirectory;
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+            terminal.error('An error occurred: ' + textStatus);
+        }
+    });
+}
+
+function buildGreetingString() {
+    var lines = [];
+    lines.push(new Date().toLocaleString());
+    lines.push("The default interactive shell is now Mosh (Mitch's Own SHell).");
+    // lines.push("(If this doesn't seem fun to you, the links above will get you to the essentials)");
+    return lines.join('\n');
+}
 
 function listDirectoriesAndFiles(terminal) {
     $.ajax({
@@ -47,6 +67,52 @@ function listDirectoriesAndFiles(terminal) {
         method: 'GET',
         success: (files) => {
             terminal.echo(files.join('\n'));
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+            terminal.error('An error occurred: ' + textStatus);
+        }
+    });
+}
+
+function changeDirectory(directory, callback) {
+    $.ajax({
+        url: `${serverBaseUrl}/change-directory`,
+        data: { directory: directory },
+        method: 'GET',
+        success: (newDirectory) => {
+            currentDirectory = newDirectory;
+            if (typeof callback === 'function') {
+                callback(newDirectory);
+            }
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+            terminal.error('An error occurred: ' + textStatus);
+        }
+    });
+}
+
+function generateAsciiArt(text) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: `${serverBaseUrl}/generate-ascii`,
+            data: { text: text },
+            method: 'GET',
+            success: (asciiArt) => {
+                resolve(asciiArt);
+            },
+            error: () => {
+                reject('An error occurred while generating ASCII art');
+            }
+        });
+    });
+}
+
+function printWorkingDirectory(terminal) {
+    $.ajax({
+        url: `${serverBaseUrl}/pwd`,
+        method: 'GET',
+        success: (directory) => {
+            terminal.echo('/Users/' + directory);
         },
         error: (jqXHR, textStatus, errorThrown) => {
             terminal.error('An error occurred: ' + textStatus);
